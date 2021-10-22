7f17be8364efe6222cce995e41cf5f99221aebe3
Serji2158 <sergey.tyschenko@gmail.com>
2021-10-22 18:35:15 +0300

    route changed

--- src/Component/auth/Auth.js
+++ src/Component/auth/Auth.js
@@ -6,7 +6,7 @@ import {
   signUpOperation,
 } from "../../redux/Auth/auth-operations";
 // import { errorSelector, loaderSelector } from "../../redux/Auth/auth-selectors";
-import AuthForm from "../auth/authForm/AuthForm";
+import AuthForm from "./authForm/AuthFormgit ";
 
 const Auth = () => {
   const dispatch = useDispatch();
